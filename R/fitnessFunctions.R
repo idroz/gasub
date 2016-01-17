9c8e10a823fa3cb4ad8e29d7de05ecbd9e379236
idroz <idrozdov@beringresearch.com>
2016-01-17 00:26:40 +0000

    removed modularity fitness

--- R/fitnessFunctions.R
+++ R/fitnessFunctions.R
@@ -14,19 +14,3 @@
   # Scoring Functions for Subgraph extraction
   return(sum(weight[x]))
 }
-
-# Network modularity maximisation
-.modularity_fitness <- function(sub, G, W){
-  igraph::V(G)$W <- W
-  sub.index <- which(sub == 1)
-  sub.g <- igraph::induced_subgraph(G, sub.index)
-
-  subgraph.clusters <- igraph::fastgreedy.community(sub.g)
-  subgraph.weights <- .get_module_weights(subgraph.clusters, W)
-  mod <- igraph::modularity(sub.g, subgraph.clusters$membership)
-  sum(mod * subgraph.weights)
-}
-
-.get_module_weights <- function(clusters, W){
-  unlist(lapply(igraph::groups(clusters), function(x) mean(W[names(W) %in% x])))
-}
