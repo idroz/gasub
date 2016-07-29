ca15b35e7dc1e252628bb88bcf41109a0d8cabe8
Ignat Drozdov <idrozdov@beringresearch.com>
2016-07-29 07:38:17 +0100

    redesigned ga algo from scratch

--- R/ga.R
+++ R/ga.R
@@ -1,397 +1,63 @@
-ga <- function(
-               fitness, ...,
-               min, max, nBits,
-               population = gabin_Population,
-               selection = gabin_lrSelection,
-               crossover = gabin_spCrossover,
-               mutation = gabin_raMutation,
-               popSize = 50,
-               pcrossover = 0.8,
-               pmutation = 0.1,
-               elitism = base::max(1, round(popSize*0.05)),
-               maxiter = 100,
-               run = maxiter,
-               maxfitness = Inf,
-               names = NULL,
-               suggestions = NULL,
-               keepBest = FALSE,
-               parallel = FALSE,
-               seed = NULL,
-               verbose = TRUE)
-{
+#' Simple implementation of a binary Genetic Algorithm
+#' @export
+#' @importFrom parallel makeCluster parApply stopCluster
+ga <- function(fitness, ..., pop.size = 50, n.bits, max.iter = 20, run = 10, pmutation = 0.01, eletism = 2, seed = 123){
+  set.seed(123)
+  cl <- makeCluster(4)
+  population <- matrix(rbinom(pop.size * n.bits, 1, 0.5), ncol = n.bits, nrow = pop.size)
 
-  eps <- sqrt(.Machine$double.eps)
-  if(missing(fitness))
-    { stop("A fitness function must be provided") }
-  if(!is.function(fitness))
-    { stop("A fitness function must be provided") }
-  if(popSize < 10)
-    { warning("The population size is less than 10.") }
-  if(maxiter < 1)
-    { stop("The maximum number of iterations must be at least 1.") }
-  if(elitism > popSize)
-    { stop("The elitism cannot be larger that population size.") }
-  if(pcrossover < 0 | pcrossover > 1)
-    { stop("Probability of crossover must be between 0 and 1.") }
-  if(is.numeric(pmutation))
-    { if(pmutation < 0 | pmutation > 1)
-        { stop("If numeric probability of mutation must be between 0 and 1.") }
-      else if(!is.function(population))
-             { stop("pmutation must be a numeric value in (0,1) or a function.") }
-  }
-  if(missing(min) & missing(max) & missing(nBits))
-    { stop("A min and max range of values (for 'real-valued' or 'permutation' GA) or nBits (for 'binary' GA) must be provided!") }
+  meanscore <- vector()
 
-  nBits <- as.vector(nBits)[1]
-  min <- max <- NA
-  nvars <- nBits
+  for (n in 1:(max.iter + 1)){
+    score <- do.call(parApply, args = list(cl = cl, X = population, MARGIN = 1, FUN = fitness, ...))
+    ordered <- order(score, decreasing = TRUE)
+    elites <- population[ordered[1 : eletism], ]
 
-  if(is.null(suggestions))
-    { suggestions <- matrix(nrow = 0, ncol = nvars) }
-  else
-    { if(is.vector(suggestions))
-        { if(nvars > 1) suggestions <- matrix(suggestions, nrow = 1)
-          else          suggestions <- matrix(suggestions, ncol = 1) }
-      else
-        { suggestions <- as.matrix(suggestions) }
-      if(nvars != ncol(suggestions))
-        stop("Provided suggestions (ncol) matrix do not match number of variables of the problem!")
-    }
+    meanscore[n] <- mean(score)
 
-  # Start parallel computing (if needed)
-  parallel <- if(is.logical(parallel))
-                { if(parallel) startParallel(parallel) else FALSE }
-              else { startParallel(parallel) }
-  on.exit(if(parallel)
-          parallel::stopCluster(attr(parallel, "cluster")) )
+    # Selection
+    prob <- abs(score) / sum(abs(score))
+    sel <- sample(1:pop.size, size = pop.size,
+                  prob = pmin(pmax(0, prob), 1, na.rm = TRUE),
+                  replace = TRUE)
+    population <- population[sel, , drop = FALSE]
 
-  fitnessSummary <- matrix(as.double(NA), nrow = maxiter, ncol = 6)
-  colnames(fitnessSummary) <- names(gaSummary(rnorm(10)))
-  bestSol <- if(keepBest) vector(mode = "list", length = maxiter)
-             else         list()
-  Fitness <- rep(NA, popSize)
 
-  object <- new("ga",
-                min = min,
-                max = max,
-                nBits = nBits,
-                names = if(is.null(names)) character() else names,
-                popSize = popSize,
-                iter = 0,
-                run = 1,
-                maxiter = maxiter,
-                suggestions = suggestions,
-                population = matrix(),
-                elitism = elitism,
-                pcrossover = pcrossover,
-                pmutation = if(is.numeric(pmutation)) pmutation else NA,
-                fitness = Fitness,
-                summary = fitnessSummary,
-                bestSol = bestSol)
+    # Crossover
+    nmating <- floor(pop.size / 2)
+    mating <- matrix(sample(1:(2 * nmating), size = (2 * nmating)), ncol = 2)
 
-  if(!is.null(seed)) set.seed(seed)
+    Crossover <- apply(mating, 1, function(x) crossover(population[x[1], ], population[x[2], ]))
+    population[mating[, 1], ]  <- t(Crossover)
 
-  # generate beginning population
-  Pop <- matrix(as.double(NA), nrow = popSize, ncol = nvars)
-  ng <- min(nrow(suggestions), popSize)
-  if(ng > 0) # use suggestion if provided
-    { Pop[1:ng,] <- suggestions }
-  # fill the rest with a random population
-  if(popSize > ng)
-    { Pop[(ng+1):popSize,] <- population(object)[1:(popSize-ng),] }
-  object@population <- Pop
+    # Eletism
+    population[ordered[1:eletism], ] <- elites
 
-  # start iterations
-  for(iter in seq_len(maxiter))
-     {
-      # evalute fitness function (when needed)
-      if(!parallel)
-        { for(i in seq_len(popSize))
-             if(is.na(Fitness[i]))
-               { Fitness[i] <- fitness(Pop[i,], ...) }
-      }
-      else
-        { Fitness <- foreach(i = seq_len(popSize), .combine = "c") %dopar%
-                     { if(is.na(Fitness[i])) fitness(Pop[i,], ...)
-                       else                  Fitness[i] }
-        }
+    cat("Iteration: ", n - 1, " | Fitness: ", meanscore[n], "\n")
 
-      fitnessSummary[iter,] <- gaSummary(Fitness)
-
-      # update object
-      object@iter <- iter
-      object@population <- Pop
-      object@fitness <- Fitness
-      object@summary <- fitnessSummary
-
-      if(keepBest)
-        # object@bestSol[[iter]] <- unique(Pop[Fitness == bestEval[iter],,drop=FALSE])
-        object@bestSol[[iter]] <- unique(Pop[Fitness == fitnessSummary[iter,1],,drop=FALSE])
-
-      if(verbose & ((iter %% 10)==0))
-          { gaMonitor(object) }
-
-      # check stopping criteria
-      if(iter > 1)
-        { if(fitnessSummary[iter,1] > fitnessSummary[iter-1,1]+eps)
-               object@run <- 1
-          else
-               object@run <- object@run + 1
-        }
-      if(object@run >= run) break
-      if(max(Fitness, na.rm = TRUE) >= maxfitness) break
-      if(object@iter == maxiter) break
-
-      # PopNew <- matrix(as.double(NA), nrow = popSize, ncol = nvars)
-      # fitnessNew <- rep(NA, popSize)
-      ord <- order(Fitness, decreasing = TRUE)
-      PopSorted <- Pop[ord,,drop=FALSE]
-      FitnessSorted <- Fitness[ord]
-
-      # selection
-      if(is.function(selection))
-        { sel <- selection(object)
-          Pop <- sel$population
-          Fitness <- sel$fitness
-        }
-      else
-        { sel <- sample(1:popSize, size = popSize, replace = TRUE)
-          Pop <- object@population[sel,]
-          Fitness <- object@fitness[sel]
-        }
-      object@population <- Pop
-      object@fitness <- Fitness
-
-      # crossover
-      if(is.function(crossover) & pcrossover > 0)
-        {
-          nmating <- floor(popSize/2)
-          mating <- matrix(sample(1:(2*nmating), size = (2*nmating)), ncol = 2)
-          for(i in seq_len(nmating))
-            { if(pcrossover > runif(1))
-                { parents <- mating[i,]
-                  Crossover <- crossover(object, parents)
-                  Pop[parents,] <- Crossover$children
-                  Fitness[parents] <- Crossover$fitness
-                }
-            }
-          object@population <- Pop
-          object@fitness <- Fitness
-        }
-
-      # mutation
-      pm <- if(is.function(pmutation)) pmutation(object) else pmutation
-      if(is.function(mutation) & pm > 0)
-        { for(i in seq_len(popSize))
-             { if(pm > runif(1))
-                 { Mutation <- mutation(object, i)
-                   Pop[i,] <- Mutation
-                   Fitness[i] <- NA
-                 }
-             }
-          object@population <- Pop
-          object@fitness <- Fitness
-        }
-
-      # elitism
-      if(elitism > 0)
-        { ord <- order(object@fitness, na.last = TRUE)
-          u <- which(!duplicated(PopSorted, margin = 1))
-          Pop[ord[1:elitism],] <- PopSorted[u[1:elitism],]
-          Fitness[ord[1:elitism]] <- FitnessSorted[u[1:elitism]]
-          object@population <- Pop
-          object@fitness <- Fitness
-        }
-
+    if (sum(diff(meanscore) == 0) + 1 >= run) break
   }
 
-  # in case of premature convergence remove NA from summary fitness evalutations
-  object@summary <- na.exclude(object@summary)
-  attr(object@summary, "na.action") <- NULL
+  stopCluster(cl)
+  res <- list(population = population, fitness = score, improvement = meanscore)
 
-  # get solution(s)
-  object@fitnessValue <- max(object@fitness, na.rm = TRUE)
-  valueAt <- which(object@fitness == object@fitnessValue)
-  solution <- object@population[valueAt,,drop=FALSE]
-  if(nrow(solution) > 1)
-    { # find unique solutions to precision given by default tolerance
-      solution <- unique(round(solution/eps)*eps, margin = 1)
-    }
-  colnames(solution) <- parNames(object)
-  object@solution <- solution
-  if(keepBest)
-    object@bestSol <- object@bestSol[!sapply(object@bestSol, is.null)]
-
-  # return an object of class 'ga'
-  return(object)
-}
-
-setClassUnion("numericOrNA", members = c("numeric", "logical"))
-
-setClass(Class = "ga",
-         representation(min = "numericOrNA",
-                        max = "numericOrNA",
-                        nBits = "numericOrNA",
-                        names = "character",
-                        popSize = "numeric",
-                        iter = "numeric",
-                        run = "numeric",
-                        maxiter = "numeric",
-                        suggestions = "matrix",
-                        population = "matrix",
-                        elitism = "numeric",
-                        pcrossover = "numeric",
-                        pmutation = "numericOrNA",
-                        fitness = "numericOrNA",
-                        summary = "matrix",
-                        bestSol = "list",
-                        fitnessValue = "numeric",
-                        solution = "matrix"
-                      ),
-         package = "gasub"
-)
-
-setGeneric(name = "parNames",
-           def = function(object, ...) { standardGeneric("parNames") }
-          )
-
-setMethod("parNames", "ga",
-function(object, ...)
-{
-  names <- object@names
-  nvars <- ncol(object@population)
-  if(length(names) == 0)
-    { names <- paste("x", 1:nvars, sep = "") }
-  return(names)
-})
-
-gaMonitor <- function(object, digits = getOption("digits"), ...)
-{
-  fitness <- na.exclude(object@fitness)
-  cat(paste("Iteration: ", object@iter,
-            " | Fitness: ", format(max(fitness), digits = digits), "\n"))
-}
-
-gaSummary <- function(x, ...)
-{
-  # compute summary for each step
-  x <- na.exclude(as.vector(x))
-  q <- fivenum(x)
-  c(max = q[5], mean = mean(x), q3 = q[4], median = q[3], q1 = q[2], min = q[1])
-}
-
-##
-## Binary GA operators
-##
-
-gabin_Population <- function(object, ...)
-{
-# Generate a random population of nBits 0/1 values of size popSize
-  population <- matrix(as.double(NA), nrow = object@popSize, ncol = object@nBits)
-  for(j in 1:object@nBits)
-     { population[,j] <- round(runif(object@popSize)) }
-  return(population)
-}
-
-gabin_lrSelection <- function(object,
-                            r = 2/(object@popSize*(object@popSize-1)),
-                            q = 2/object@popSize, ...)
-{
-# Linear-rank selection
-# Michalewicz (1996) Genetic Algorithms + Data Structures = Evolution Programs. p. 60
-  rank <- (object@popSize+1) - rank(object@fitness, ties.method = "random")
-  prob <- q - (rank-1)*r
-  sel <- sample(1:object@popSize, size = object@popSize,
-                prob = pmin(pmax(0, prob), 1, na.rm = TRUE),
-                replace = TRUE)
-  out <- list(population = object@population[sel,,drop=FALSE],
-              fitness = object@fitness[sel])
-  return(out)
-}
-
-gabin_nlrSelection <- function(object, q = 0.25, ...)
-{
-# Nonlinear-rank selection
-# Michalewicz (1996) Genetic Algorithms + Data Structures = Evolution Programs. p. 60
-  rank <- (object@popSize + 1) - rank(object@fitness, ties.method = "random")
-  prob <- q*(1-q)^(rank-1)
-  sel <- sample(1:object@popSize, size = object@popSize,
-                prob = pmin(pmax(0, prob), 1, na.rm = TRUE),
-                replace = TRUE)
-  out <- list(population = object@population[sel,,drop=FALSE],
-              fitness = object@fitness[sel])
-  return(out)
-}
-
-gabin_rwSelection <- function(object, ...)
-{
-# Proportional (roulette wheel) selection
-  prob <- abs(object@fitness)/sum(abs(object@fitness))
-  sel <- sample(1:object@popSize, size = object@popSize,
-                prob = pmin(pmax(0, prob), 1, na.rm = TRUE),
-                replace = TRUE)
-  out <- list(population = object@population[sel,,drop=FALSE],
-              fitness = object@fitness[sel])
-  return(out)
-}
-
-gabin_tourSelection <- function(object, k = 3, ...)
-{
-# (unbiased) Tournament selection
-  sel <- rep(NA, object@popSize)
-  for(i in 1:object@popSize)
-     { s <- sample(1:object@popSize, size = k)
-       sel[i] <- s[which.max(object@fitness[s])]
-     }
-  out <- list(population = object@population[sel,,drop=FALSE],
-              fitness = object@fitness[sel])
-  return(out)
-}
-
-gabin_spCrossover <- function(object, parents, ...)
-{
-# Single-point crossover
-  fitness <- object@fitness[parents]
-  parents <- object@population[parents,,drop = FALSE]
-  n <- ncol(parents)
-  children <- matrix(as.double(NA), nrow = 2, ncol = n)
-  fitnessChildren <- rep(NA, 2)
-  crossOverPoint <- sample(0:n, size = 1)
-  if(crossOverPoint == 0)
-    { children[1:2,] <- parents[2:1,]
-      fitnessChildren[1:2] <- fitness[2:1] }
-  else if(crossOverPoint == n)
-         { children <- parents
-           fitnessChildren <- fitness }
-       else
-         { children[1,] <- c(parents[1,1:crossOverPoint],
-                           parents[2,(crossOverPoint+1):n])
-           children[2,] <- c(parents[2,1:crossOverPoint],
-                           parents[1,(crossOverPoint+1):n])
-         }
-  out <- list(children = children, fitness = fitnessChildren)
-  return(out)
+  return(res)
 }
 
 
-gabin_uCrossover <- function(object, parents, ...)
-{
-# Uniform crossover
-  parents <- object@population[parents,,drop = FALSE]
-  n <- ncol(parents)
-  u <- runif(n)
-  children <- parents
-  children[1:2, u > 0.5] <- children[2:1, u > 0.5]
-  out <- list(children = children, fitness = rep(NA,2))
-  return(out)
+crossover <- function(x, y){
+  area <- runif(length(x)) > 0.5
+  x[area] <- y[area]
+  child <- x
+  return(child)
 }
 
-gabin_raMutation <- function(object, parent, ...)
-{
-# Uniform random mutation
-  mutate <- parent <- as.vector(object@population[parent,])
-  n <- length(parent)
-  j <- sample(1:n, size = 1)
-  mutate[j] <- abs(mutate[j]-1)
-  return(mutate)
+#' @importFrom igraph induced_subgraph neighborhood vcount
+fitness <- function(sub, G, W){
+  igraph::V(G)$W <- W
+  sub.index <- which(sub == 1)
+  sub.g <- induced_subgraph(G, sub.index)
+
+  neighbor.weights <- unlist(lapply(neighborhood(sub.g, 1), function(x, weight = V(sub.g)$W) sum(weight[x]) ))
+  sum(V(sub.g)$W * neighbor.weights) / sqrt(vcount(sub.g))
 }
--- R/gasub.R
+++ R/gasub.R
@@ -4,50 +4,35 @@
 #' @param weights           named vector of node weights. Vector names must match igraph node
 #'                          NAME attribute. In an input graph, nodes not found in V will be
 #'                          assigned a weight of 0.
-#' @param activity.fun  s   coring function to be maximised
 #' @param run               the number of consecutive generations without any improvement
 #'                          in the best finess value before the GA is stopped
-#' @param init.threshold    initial weight threshold above which weights are significant.
-#'                          optional parameter that speeds up training. If omitted, algorithm
-#'                          still converges accurately, albeit slower.
-#' @param maxiter           the maximum number of iterations to run before the GA search
+#' @param max.iter           the maximum number of iterations to run before the GA search
 #'                          is halted.
-#' @param parallel          logical argument specifying if parallel computing should be
-#'                          used (‘TRUE’) or not (‘FALSE’, default) for evaluating the
-#'                          fitness function. This argument could also be used to specify
-#'                          the number of cores to employ; by default, this is taken from
-#'                          ‘detectCores’. Finally, the functionality of parallelization
-#'                          depends on system OS: on Windows only 'snow' type
-#'                          functionality is available, while on Unix/Linux/Mac OSX both
-#'                          'snow' and 'multicore' (default) functionalities are
-#'                          available.
-#' @param ...               additional arguments to be passed to the ga() function
 #' @export
 
-gaSubgraph <- function(graph, weights, activity.fun = .weight_fitness, run = 50, init.threshold = NULL, maxiter = 100000, parallel = 2, ...){
-  .check_input_params(graph, weights)
+gaSubgraph <- function(graph, weights, run = 50, max.iter = 100000){
 
   # Only keep seed nodes that are present in the graph
-  weights <- weights[ names(v) %in% igraph::V(graph)$name ]
+  weights <- weights[ names(weights) %in% V(graph)$name ]
 
   # Assign a weight 0 to all nodes in the graph.
   # This ensures that nodes without an assigned weight have a default weight of 0
-  full.seed = rep(0, igraph::vcount(graph))
-  names(full.seed) <- igraph::V(graph)$name
+  full.seed <- rep(0, vcount(graph))
+  names(full.seed) <- V(graph)$name
 
   # Find locations of input seed nodes in the graph
-  ix <- match(names(weights), igraph::V(graph)$name)
+  ix <- match(names(weights), V(graph)$name)
   # Remove NA indeces
   ix <- ix[!is.na(ix)]
 
   full.seed[ix] <- weights
 
-  suggestions <- NULL
-  if(!is.null(init.threshold)) suggestions <- as.numeric(full.seed >= init.threshold)
 
-  GA <- ga(fitness = activity.fun, G = graph, W = full.seed, nBits = igraph::vcount(graph), seed = 123, run = run, maxiter = maxiter, parallel = parallel, suggestions = suggestions)
 
-  sub.g <- igraph::induced_subgraph(graph, which(GA@solution[1,]==1))
+  GA <- ga(fitness, G = graph, W = full.seed, n.bits = vcount(graph), pop.size = 30, max.iter = max.iter, run = run, seed = 123)
+
+  sub.g <- induced_subgraph(graph, which(GA$population[which.max(GA$fitness), ] == 1))
+
 
   return(sub.g)
 }
