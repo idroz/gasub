bb46eb30f1a21de70a93374ded3a248d184ecb84
Ignat Drozdov <idrozdov@beringresearch.com>
2016-08-03 07:55:54 +0100

    changed crossover to uniform region

--- inst/julia/Crossover.jl
+++ inst/julia/Crossover.jl
@@ -1,5 +1,11 @@
 function Crossover(x, y)
-  area = find(rand(Uniform(), 1, length(x)) .> 0.5)
-  x[area] = y[area]
+  #area = find(rand(Uniform(), 1, length(x)) .> 0.5)
+  #x[area] = y[area]
+  #x
+
+  crossover_length = reshape(sample(1:(round(Int8, length(x)/2)), 1), 1)[1]
+  crossover_position = reshape(sample(1:(length(x) - crossover_length), 1))[1]
+
+  x[crossover_position:(crossover_position + crossover_length)] = y[crossover_position:(crossover_position + crossover_length)]
   x
 end
