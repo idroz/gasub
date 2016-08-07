90ac1d10feb8c5b1d3431e3e47e7e94fca83b076
Ignat Drozdov <idrozdov@beringresearch.com>
2016-08-07 14:40:06 +0100

    code cleanup

--- inst/julia/Crossover.jl
+++ inst/julia/Crossover.jl
@@ -1,8 +1,4 @@
 function Crossover(x, y)
-  #area = find(rand(Uniform(), 1, length(x)) .> 0.5)
-  #x[area] = y[area]
-  #x
-
   crossover_length = reshape(sample(1:(round(Int64, length(x)/2)), 1), 1)[1]
   crossover_position = reshape(sample(1:(length(x) - crossover_length), 1))[1]
 
